f2fe6b95f993304a016cfa739dcf178fd7fee83c
Javier <jsespin1@uc.cl>
2016-09-07 17:20:08 -0300

    commit error al trabajar en master

---
+++ db/migrate/20160907201948_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.integer :money
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
